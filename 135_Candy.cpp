05756ee8d96d31fe6810595b5c863392a27bd4ea
southmountain <1343165348@qq.com>
2017-01-04 23:55:24 +0800

    update

---
+++ 135_Candy.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        int size=ratings.size();
+        vector<int>num(size,1);
+        for(int i=1;i<size;i++)
+        {
+            if(ratings[i]>ratings[i-1])
+            num[i]=num[i-1]+1;
+        }
+        for(int i=size-2;i>=0;i--)
+        {
+            if(ratings[i]>ratings[i+1])
+            num[i]=max(num[i+1]+1,num[i]);
+        }
+        int result=0;
+        for(int i=0;i<size;i++)
+        {
+            result+=num[i];
+        }
+        return result;
+    }
+};
